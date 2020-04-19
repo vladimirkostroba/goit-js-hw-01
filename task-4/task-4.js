2472fd46d31c4581dbb0a8efa90b0bb150f34e9a
Vladimir Kostroba <vladimirkostroba22@gmail.com>
2020-04-19 20:40:49 +0300

    fertig

--- task-4/task-4.js
+++ task-4/task-4.js
@@ -1,7 +1,19 @@
 let credits = 23580;
 const pricePerDroid = 3000;
-let message = prompt('сколько единиц Вы желаете приобрести');
+let message = prompt('Cколько единиц Вы желаете приобрести?');
 
 if (message === null) {
-  message = 'отменено пользователем';
-} else if (message)
+  message = 'Отменено пользователем!';
+  console.log(message);
+} else {
+  message = Number(message);
+}
+
+const totalPrice = message * pricePerDroid;
+
+if (totalPrice > credits) {
+  console.log('Недостаточно средств на счету!');
+} else if (message >= 0) {
+  credits -= totalPrice;
+  alert(`Вы купили ${message} дроидов, на счету осталось ${credits} кредитов.`);
+}
